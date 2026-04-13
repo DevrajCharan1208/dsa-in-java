c378ce0009115c4d410cea5b47398fc81d3dcbfd
DevrajCharan1208 <devrajcharan1208@gmail.com>
2026-04-13 08:07:21 +0530

    Add 2 new files

---
+++ basics/binomialCoefficient.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class binomialCoefficient{
+
+    static int factorial(int n){
+    int fact = 1;
+    for (int i=1; i<=n; i++){
+        fact *= i;
+    }
+    return fact;
+    }
+
+    static int binCoeff(int n, int r){
+        int n_fact = factorial(n);
+        int r_fact = factorial(r);
+        int nr_fact = factorial(n-r);
+        int coefficient = n_fact/(r_fact*nr_fact);
+
+        return coefficient;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value for n : ");
+        int n = sc.nextInt();
+        System.out.print("Enter value for r : ");
+        int r = sc.nextInt();
+
+        System.out.println(binCoeff(n, r));
+    }
+}
---
+++ basics/factorial.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class factorial{
+
+static int factorial(int n){
+    int fact = 1;
+
+    for (int i=1; i<=n; i++){
+
+        fact *= i;
+    }
+
+    return fact;
+}
+
+
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number to calculate factorail : ");
+    int num = sc.nextInt();
+    int fact = factorial(num);
+    System.out.println("Factorial of "+ num + " is "+ fact);
+}
+}
