cfab52f9bb1c960c6bfa117561be015007280167
Devraj Charan <devrajcharan1208@gmail.com>
2026-05-08 12:39:35 +0530

    Add new file

---
+++ Recursion/R08NoConsecutiveOnes.java
@@ -0,0 +1,18 @@
+public class R08NoConsecutiveOnes{
+
+    public static void noConsecutiveOnes(int n,int lastPlace, String str ){
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+
+        noConsecutiveOnes(n-1, 0, str+"0");
+        if(lastPlace == 0){
+            noConsecutiveOnes(n-1, 1, str+"1");
+        }
+    }
+
+    public static void main(String[] args) {
+        noConsecutiveOnes(5, 0, "");
+    }
+}
