d7cb60bce3b77fba3991b75da9c36c298bebce7e
Devraj Charan <devrajcharan1208@gmail.com>
2026-05-27 11:17:00 +0530

    feat: add implementations for binary tree diameter calculation using O(n^2) and O(n) approaches

---
+++ BinaryTrees/TR05DiameterOfTree.java
@@ -0,0 +1,52 @@
+public class TR05DiameterOfTree{
+    @SuppressWarnings("unused")
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh,rh)+1;
+    }
+
+    public static int diameter(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        int rootH = lh+rh+1;
+        int lDiam = diameter(root.left);
+        int rDiam = diameter(root.right);
+
+        return Math.max(rootH,Math.max(lDiam,rDiam));
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left =new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right =new Node(7);
+        root.right.right.right = new Node(8);
+
+        System.out.println(diameter(root));
+    }
+}
---
+++ BinaryTrees/TR06DiameterOfTree2.java
@@ -0,0 +1,54 @@
+public class TR06DiameterOfTree2{
+    @SuppressWarnings("unused")
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Info{
+        int diam;
+        int ht;
+
+        Info(int diam, int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    static Info diameter(Node root){
+        if(root==null){
+            return new Info(0,0);
+        }
+
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diameter = Math.max(leftInfo.diam,Math.max(rightInfo.diam,leftInfo.ht+rightInfo.ht+1));
+        int ht = Math.max(leftInfo.ht,rightInfo.ht)+1;
+
+        return new Info(diameter,ht);
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left =new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right =new Node(7);
+        root.right.right.right = new Node(8);
+
+        System.out.println(diameter(root).diam+ " "+ diameter(root).ht);
+    }
+}
