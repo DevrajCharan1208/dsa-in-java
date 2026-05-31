b8c2bb686e229e4aa20c464ebe0f378eda35b340
Devraj Charan <devrajcharan1208@gmail.com>
2026-05-31 08:39:41 +0530

    feat: implement binary search tree with insert, delete, and inorder traversal methods

---
+++ BinarySearchTrees/BST02DeleteANode.java
@@ -0,0 +1,86 @@
+public class BST02DeleteANode{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    private  static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data>val){
+            root.left =  insert(root.left, val);
+        }else{
+            root.right = insert(root.right,val);
+        }
+        return root;
+    }
+
+    private static Node delete(Node root, int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        }else if(root.data > val){
+            root.left = delete(root.left, val);
+        }else{
+            //case1 - No child.
+            if(root.left == null || root.right == null){
+                return null;
+            }
+
+            //case2 - single child.
+            if(root.left == null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+
+            //case3 - two child.
+            Node IS  = inorderSUccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right,IS.data);
+        }
+
+        return root;
+    }
+
+    private static Node inorderSUccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    private  static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+
+    public static void main(String args[]){
+        int[] values = {5,1,3,4,2,7};
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root,values[i]);
+        }
+
+        inorder(root);
+        System.out.println("");
+
+        delete(root, 4);
+        inorder(root);
+        
+        
+    }   
+}
